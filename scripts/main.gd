2cd3551311cce452a94dce6acf627791c2e33285
Felipe Morales Ortiz <moraleitoryt@gmail.com>
2026-09-02 17:33:02 -0500

    feat: update aim reticle to follow raycast

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,34 +1,40 @@
 extends Node3D
+
 @onready var player = $Player
 @onready var crosshair = $UI/Crosshair
 @onready var crosshair_hit = $UI/Crosshair_hit
 
+# Reducir offset_x mueve la cruz a la DERECHA (30.0)
+# Reducir offset_y mueve la cruz ABAJO (54.0)
+@export var offset_x: float = 30.0
+@export var offset_y: float = 50.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-	crosshair.position.x = get_viewport().size.x /2 -64
-	crosshair.position.y = get_viewport().size.y /2 -64
-	crosshair_hit.position.x = get_viewport().size.x /2 -64
-	crosshair_hit.position.y = get_viewport().size.y /2 -64
-	
-	# Al comenzar, mostramos el normal y ocultamos el de impacto
+	# Evita que las imágenes de la UI bloqueen el movimiento del mouse
+	crosshair.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	crosshair_hit.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+	alinear_reticula()
+
 	crosshair.visible = true
 	crosshair_hit.visible = false
-	
+
 	player.target_hit.connect(crosshair_hit_effect)
 
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func alinear_reticula() -> void:
+	var screen_size = Vector2(get_viewport().size)
+	var center = screen_size / 2.0
 	
+	var pos_final = Vector2(center.x - offset_x, center.y - offset_y)
+	
+	crosshair.position = pos_final
+	crosshair_hit.position = pos_final
+
 func crosshair_hit_effect() -> void:
 	crosshair.visible = false
 	crosshair_hit.visible = true
-	
+
 	await get_tree().create_timer(0.1).timeout
-	
+
 	crosshair.visible = true
 	crosshair_hit.visible = false
