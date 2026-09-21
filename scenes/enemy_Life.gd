8230010720f629258edb1a50f526b95c1171a2ba
Andres061308 <andreschiquiza08@gmail.com>
2026-09-20 22:44:01 -0500

    Creacion EnemyLife
    Clase que extiende de Enemy  que sobreescribe hit() para restar vida en lugar de matar directamente

---
+++ scenes/enemy_Life.gd
@@ -0,0 +1,26 @@
+extends Enemy
+class_name EnemyLife
+
+@export var vida_maxima: int = 3
+
+var vida_actual: int
+
+
+func _ready() -> void:
+	super._ready()
+	vida_actual = vida_maxima
+
+func hit() -> void:
+	vida_actual -= 1
+	enemy_damaged.emit(vida_actual, vida_maxima)
+	
+	if vida_actual <= 0:
+		registrar_puntos()
+		morir()
+	else:
+		recibir_golpe()
+		
+func recibir_golpe() -> void:
+	var tween = create_tween()
+	tween.tween_property(mesh, "modulate", Color(1, 0.3, 0.3), 0.05)
+	tween.tween_property(mesh, "modulate", Color.WHITE, 0.15)
