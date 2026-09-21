1db9e7171ddbe4cfa3bfcfd160838ad58d5a3ede
Andres061308 <andreschiquiza08@gmail.com>
2026-09-20 22:35:39 -0500

    Cambio en la logica de la funcion hit() en Enemy.gd

--- scripts/gameplay/enemy.gd
+++ scripts/gameplay/enemy.gd
@@ -1,14 +1,16 @@
 extends Area3D
+class_name Enemy
+
 signal enemy_hit
+signal enemy_damaged(vida_restante: int, vida_maxima: int)
 
 @onready var mesh: MeshInstance3D = $MeshInstance3D
-
 @onready var particles: GPUParticles3D = $GPUParticles3D
-
 @onready var collision: CollisionShape3D = $CollisionShape3D
 
 
 var tiempo_spawn: int = 0
+var puntos_base: int = 20
 
 
 
@@ -20,12 +22,19 @@ func _ready() -> void:
 
 
 func hit() -> void:
-	var puntos_base = int(20 / scale.x)
+	registrar_puntos()
+	morir()
+	
+	
+func registrar_puntos() -> void:
+	var base = int(puntos_base/scale.x)
 	var tiempo_vivo = (Time.get_ticks_msec() - tiempo_spawn) / 1000.0
 	var multiplicador = clamp(2.0 - (tiempo_vivo / 5.0), 0.5, 2.0)
 	var puntos_finales = int(puntos_base * multiplicador)
 	
 	Estadisticas.registrar_acierto(puntos_finales)
+
+func morir() -> void:
 	mesh.visible = false
 	collision.disabled = true
 	particles.emitting = true
