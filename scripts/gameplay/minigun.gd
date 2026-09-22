3262d068cd7c26a83087a75526da0b0740b30011
Panteve <diegorojer@gmail.com>
2026-09-21 21:43:51 -0500

    Se agrego el asset de minigun en formato .glb lo que hizo que se comprimiera y ya esta puesto en su escena correspondiente

--- scripts/gameplay/minigun.gd
+++ scripts/gameplay/minigun.gd
@@ -6,15 +6,15 @@ var shots_fired_in_burst: int = 0
 
 
 func start_fire() -> void:
-	anim_player.play("rig|Fire_Start")
+	anim_player.play("rig|rig|Fire_Start")
 	await anim_player.animation_finished
 
 func stop_fire() -> void:
 	super.stop_fire()
-	anim_player.play("rig|Fire_Stop")
+	anim_player.play("rig|rig|Fire_Stop")
 	
 func play_idle() -> void:
-	if anim_player.current_animation not in ["rig|Fire_Stop", "rig|Fire_Start", "rig|Fire", "rig|Inspect"]:
+	if anim_player.current_animation not in ["rig|rig|Fire_Stop", "rig|rig|Fire_Start", "rig|rig|Fire", "rig|rig|Inspect"]:
 		anim_player.play(anim_idle)
 
 func reset_burst() -> void:
